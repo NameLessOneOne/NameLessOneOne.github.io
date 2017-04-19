98eefa7bf1e1ee64d2fb37c18f9f203be87aa890
NameLessOneOne <kirilkaoneone@gmail.com>
2017-04-19 23:42:23 +0300

    js toggle

--- webCV/js/script.js
+++ webCV/js/script.js
@@ -1,15 +1,40 @@
+var buttonShowHide = document.querySelectorAll('.extend'),
+    blockShowHide = document.querySelectorAll('.extend__text');
+
+buttonShowHide.forEach(function(item, i) {
+    buttonShowHide[i].addEventListener('click', collapse);
+    item.id = "collapseButton" + i;
+});
+blockShowHide.forEach(function(item, i) {
+    item.id = "showHideBlock" + i;
+});
+
+function collapse(i) {
+    var collapseButtonId = i.target.id,
+        tCardButton = document.getElementById(collapseButtonId),
+        getIdNumber = collapseButtonId.match(/\d+/),
+        showHideBlock = document.getElementById('showHideBlock' + getIdNumber[0]),
+        computedStyleMaxHeight = getComputedStyle(showHideBlock);
+
+    if (computedStyleMaxHeight.maxHeight === '0px') {
+        showHideBlock.classList.add("extend__text_animationOn");
+    } else {
+        showHideBlock.classList.remove("extend__text_animationOn");
+    }
+}
+
 $(document).ready(function() {
 
-    $('.extend__text').slideToggle(1000);
+    // $('.extend__text').slideToggle(1000);
+    //
+    // $('.extend').click(function() {
+    //     $('.extend__text').slideToggle(500);
+    //     return false;
+    // });
 
-    $('.extend').click(function() {
-        $('.extend__text').slideToggle(500);
-        return false;
-    });
-
-    function hide(page) {
-        $(page).find('.extend__text').slideUp(500);
-    }
+    // function hide(page) {
+    //     $(page).find('.extend__text').slideUp(500);
+    // }
 
     $("[data-fancybox]").fancybox({
         // Options will go here
@@ -24,21 +49,4 @@ $(document).ready(function() {
         selectedAttraction: 0.01,
     friction: 0.15
     });
-
-    // $('.personalInfo__img').on('click', function() {
-    //     var img = $(this);
-    //
-    //
-    // });
-
-
-    //   $('.personalInfo__img').on('click', function(){
-    //     $('.personalInfo__img').toggle(true, function(){
-    //       $('.personalInfo__img').animate({ "left": "50px"}, "slow" );
-    //     $('.personalInfo__img').animate({ "width": "+=50px",  "height": "+=50px"}, "slow" );
-    //   });
-    //
-    // });
-
-
 });
